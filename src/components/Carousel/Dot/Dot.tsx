bebb40f7e8814e8e51049ffc0416f31b570a3bb6
SgtGarro <marcelogarro137@gmail.com>
2024-04-13 00:39:06 -0500

    feat #4: maquetar seccion reviews y crear componente carrusel

---
+++ src/components/Carousel/Dot/Dot.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import type { FC, HTMLAttributes } from 'react'
+
+import styles from './Dot.module.css'
+
+interface DotsProps extends HTMLAttributes<HTMLButtonElement> {
+  currentSlide: number
+  index: number
+}
+const Dot: FC<DotsProps> = ({ currentSlide, index, ...props }) => {
+  return (
+    <button
+      className={`${styles.dot} ${currentSlide === index ? styles.activeDot : styles.dot}`}
+      {...props}
+    />
+  )
+}
+
+export default Dot
